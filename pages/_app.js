db425d7299cce299763f8afe979bf104ce0bc6f7
Sean Anih <sean.anih@gmail.com>
2023-02-12 17:19:38 -0500

    added NextAuth to app; still need to protect routes

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import Navbar from '../components/Navbar';
 import '../styles/globals.css';
+import { SessionProvider } from 'next-auth/react';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 	return (
-		<>
+		<SessionProvider session={session}>
 			<Navbar />
 			<Component {...pageProps} />
-		</>
+		</SessionProvider>
 	);
 }
 
