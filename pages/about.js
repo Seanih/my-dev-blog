359e20112ef881569b5de45453f9dc8fb1b34e6c
Sean Anih <sean.anih@gmail.com>
2023-02-27 13:18:18 -0500

    finished About page

--- pages/about.js
+++ pages/about.js
@@ -37,10 +37,10 @@ function About() {
 							My interest in programming started summer of 2019, and it all
 							stemmed from researching more valuable skills I could learn that
 							involved working with computers. I played or worked with a variety
-							of tech my entire life, so it felt like a natural and logical
-							next-step to learn how to create software myself. I spent about a
-							week trying to figure out what route I wanted to take to learn
-							programming and ultimately decided to attend an online bootcamp.
+							of tech my entire life, so it felt like a natural next-step to
+							learn how to create software myself. I spent about a week trying
+							to figure out what route I wanted to take to learn programming and
+							ultimately decided to attend an online bootcamp.
 						</p>
 						<p>
 							During the first 2 months, I struggled a lot more than I expected
@@ -61,15 +61,15 @@ function About() {
 							refamiliarizing myself with the basics because of my break, but
 							once I became comfortable again I moved on to learning React. I
 							really fell in love with JSX and how much faster React was at
-							building UIs compared to vanilla JS, so I dug in as best as I
-							could, built some portfolio projects and started applying to jobs
-							in Feb 2021.
+							building UIs compared to vanilla JS, so I invested time in a few
+							courses, built some portfolio projects and started applying to
+							jobs in Feb 2021.
 						</p>
 					</section>
 					<section id='failures'>
 						<h3 className='text-center mb-4'>My Failures</h3>
 						<p>
-							I got nothing but rejections and silence the 3 months I was
+							I got nothing but rejections and silence the few months I was
 							applying, and the closest I got to anything meaningful was an
 							interview with a small company that sold software; it was a
 							product support specialist position that was basically a customer
@@ -95,16 +95,17 @@ function About() {
 							It felt good for a while doing what I was familiar with, and at
 							times it made me regret trying to do anything else in the first
 							place. But it didn't take long for me to start resenting myself,
-							and it's because I{' '}
-							<strong>
-								<i>truly</i>
-							</strong>{' '}
-							wanted to be a programmer but felt like I would never be good
-							enough. Then it started to affect my drive as an engineer, and for
-							a short period of time I had no idea what I was going to do and I
-							felt miserable. So after spending some time really assessing my
-							situation, I decided that if I was going to be miserable, I might
-							as well feel it going after what I{' '}
+							and it's because I really just wanted to be a programmer but felt
+							like I would never be good enough. I forgot to say earlier that I
+							had short stints learning other technologies like MySQL, Python,
+							and C#, but every time I ran into a bug or installation issue I
+							couldn't understand I would just get overwhelmed and pivot; that
+							negative cycle only made me feel worse and I didn't even realize I
+							was trapping myself in it. Then it started to affect my drive as
+							an audio engineer, and for a short time I had no idea what I was
+							going to do and felt miserable. So after spending some time really
+							assessing my situation, I decided that if I was going to be
+							miserable, I might as well feel it going after what I{' '}
 							<strong>
 								<i>really</i>
 							</strong>{' '}
@@ -114,6 +115,44 @@ function About() {
 					</section>
 					<section id='turning_point'>
 						<h3 className='text-center mb-4'>My Turning Point</h3>
+						<p>
+							In June of 2022, I swore to myself that I would learn blockchain
+							development. I was exposed to the technology when I started
+							dabbling in crypto currencies towards the end of 2021, and despite
+							what was happening to the crypto markets I could still see the
+							opportunities in the underlying technology. It seemed so advanced
+							and I had no idea how something like that could work, so I figured
+							that would be the best place to start. I did some research, found
+							some courses from a few good sources and started learning how the
+							blockchain and smart contracts work.
+						</p>
+						<p>
+							My main resource was a more expensive but really effective course
+							that didn't have a Q&A section, and I chose it because I needed to
+							experience being forced to find my own solutions. I was scared at
+							first, but it turned out being the best decision because I slowly
+							learned that I was able to actually solve problems myself; my
+							favorites took seconds or minutes to solve, others took hours or
+							even longer, but with enough effort and patience I was able to
+							solve every single issue I ran into. I started learning faster
+							because my confidence was growing, and in turn I learned how to
+							solve bigger and more complex problems.
+						</p>
+						<p>
+							Once I became comfortable with the fundamentals of full-stack
+							blockchain development, I knew I would be able learn off-chain
+							full-stack as well. I started by learning how to interact with
+							NoSQL databases, and once I understood that I moved on to SQL
+							databases, so now I feel comfortable working with both. I have a
+							significantly better understanding of programming and how data
+							flows through an application, and my foundation is solid enough
+							for me to expand and - given enough time - be able to build any
+							application I would need to. <strong>I love programming</strong>,
+							I love how{' '}
+							<strong>it made me grow and change for the better</strong>, and
+							I'm ready to capitalize on all of the opportunities it will bring.
+						</p>
+						<div className='h-[2px] w-3/4 my-10 mx-auto bg-slate-800'></div>
 					</section>
 				</div>
 			</main>
